0c95bb872b22d5cd54ee66fba3c01a28cfb04012
Vic <vaa0518@gmail.com>
2022-07-03 11:57:23 -0700

    [finished simple webpack config, started src tree and building front end pipeline]

---
+++ src/WelcomeBanner/welcomeBanner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Welcome = () => {
+    return (
+	<div className= 'rectangle' style={backgroundColor:#B5E5CF}>
+		<h3>Welcome!</h3>
+	</div>
+	);
+};
+
+export default Welcome;
+
